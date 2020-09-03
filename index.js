4439bc083a6e09c200224889793571bd3c8959a7
Santoo-san <67684816+Santoo-san@users.noreply.github.com>
2020-09-03 10:46:00 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+console.log("Hallo Winc Academy studenten");
+// dit is een grote som
+let multiplication = 1230941 * 1823794;
+console.log(multiplication);
+
+//if - else statement
+let youLikeMeat = true;
+
+if (youLikeMeat) {
+  console.log("Here is the meaty menu...");
+}
+console.log(1 + 0);
+if (7 > 5) {
+  console.log("Here is the meaty menu...");
+}
+console.log(1 + 1);
+if (5 > 7) {
+  console.log("Here is the meaty menu...");
+} else {
+  console.log("Here is the vegan menu");
+}
+console.log(1 + 2);
+
+//else-if statement
+let myAge = 9;
+
+if (myAge > 39) {
+  console.log("you are older than me!");
+} else if (myAge > 21) {
+  console.log("you are younger thane me, but still adult");
+} else if (myAge > 10) {
+  console.log("you are a teenager");
+} else {
+  console.log("you are a child");
+}
+const isFemale = false;
+if (isFemale == true) {
+  console.log("please enter");
+} else console.log("get out of my face, you man");
+console.log(1 + 3);
+
+const driverStatus = "piet";
+if (driverStatus == "bob") {
+  console.log("no beers for you, you may drive");
+} else console.log("take a beer, and a taxi");
